5f7795e79490d0f35b82f5daf97dd658214fa13e
bry92 <pagebrian49@gmail.com>
2026-07-22 12:39:20 -0700

    Delete app/events/[slug] directory

--- app/events/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-import PageShell from '@/app/components/PageShell'
-import ResourceDetail from '@/app/components/ResourceDetail'
-
-export default function Page({ params }: { params: { slug: string } }) {
-  return <PageShell><ResourceDetail kind="events" slug={params.slug} /></PageShell>
-}
